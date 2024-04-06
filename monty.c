7b769a02089fa4e6b252c4451517f6de13243707
root <bradenpchance6@gmail.com>
2024-04-06 20:19:39 +0000

    switches usage error to fprintf instead of printf

--- monty.c
+++ monty.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("USAGE: %s file\n", argv[0]);
+		fprintf(stderr, "USAGE: %s file\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
 	FILE *file = fopen(argv[1], "r");

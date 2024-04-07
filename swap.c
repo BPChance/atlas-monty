89bbfce00990b001c4ad400995581db39ea67b6c
root <bradenchance6@gmail.com>
2024-04-07 22:31:22 +0000

    added swap opcode in main - added swap function

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to a pointer to the stack
+ * @line_number: line_number
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	/* store pointers to the top two nodes */
+	stack_t *top = *stack;
+	stack_t *second = (*stack)->next;
+	int temp;
+	/* check if stack contains at least two elements */
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	/* swap values of top two nodes */
+	temp = top->n;
+	top->n = second->n;
+	second->n = temp;
+}

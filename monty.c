d013bb50066163ae52c542434dcb2cbf9b7a3239
root <bradenpchance6@gmail.com>
2024-04-07 09:50:31 +0000

    removed code that was messing up the push

--- monty.c
+++ monty.c
@@ -13,21 +13,6 @@ stack_t *stack = NULL;
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	/* pointer to string representation of the integer */
-	char *arg_str;
-	/* int value extracted from the string */
-	int value;
-
-	arg_str = strtok(NULL, " \n");
-
-	if (arg_str == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	/* convert string to integer */
-	value = atoi(arg_str);
-
 	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
@@ -36,7 +21,7 @@ void push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = value;
+	new_node->n = line_number;
 	new_node->prev = NULL;
 	new_node->next = *stack;
 

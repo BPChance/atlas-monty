ef54e89dbdf60a601d0622b657d51f83507de283
root <bradenchance6@gmail.com>
2024-04-07 22:36:46 +0000

    fixing swap function

--- swap.c
+++ swap.c
@@ -8,8 +8,8 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	/* store pointers to the top two nodes */
-	stack_t *top = *stack;
-	stack_t *second = (*stack)->next;
+	stack_t *top;
+	stack_t *second;
 	int temp;
 	/* check if stack contains at least two elements */
 	if (*stack == NULL || (*stack)->next == NULL)
@@ -17,6 +17,10 @@ void swap(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	/* store pointers to the top two nodes */
+	top = *stack;
+	second = (*stack)->next;
 	/* swap values of top two nodes */
 	temp = top->n;
 	top->n = second->n;

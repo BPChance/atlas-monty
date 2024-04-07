de29f85b64e0cea884ef57a110f702c5bd93f801
root <bradenpchance6@gmail.com>
2024-04-07 01:55:18 +0000

    fixed push to get negative numbers

--- monty.c
+++ monty.c
@@ -11,17 +11,17 @@ stack_t *stack = NULL;
  * @line_number: line number
  * Return: void
  */
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, int value)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
 	{
-		printf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = line_number;
+	new_node->n = value;
 	new_node->prev = NULL;
 	new_node->next = *stack;
 

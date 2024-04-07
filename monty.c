962ce1239b12b769f5e0a0e1cd005a23ea95a76f
root <bradenpchance6@gmail.com>
2024-04-07 02:23:40 +0000

    Included prototypes in header, fixed push function to properly handle negative integers

--- monty.c
+++ monty.c
@@ -11,8 +11,23 @@ stack_t *stack = NULL;
  * @line_number: line number
  * Return: void
  */
-void push(stack_t **stack, int value)
+void push(stack_t **stack, unsigned int line_number)
 {
+	/* pointer to string representation of the integer */
+	char *arg_str;
+	/* int value extracted from the string */
+	int value;
+
+	arg_str = strtok(NULL, " \n");
+
+	if (arg_str == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	/* convert string to integer */
+	value = atoi(arg_str);
+
 	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (new_node == NULL)
--- monty.h
+++ monty.h
@@ -34,6 +34,9 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+int main(int argc, char *argv[]);
+void free_stack(stack_t *stack);
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
 
 #endif
